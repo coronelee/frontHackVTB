d84178fe8851eef9dc6703fb85e3b0e3f1aba384
coronelee <a.tirki90@gmail.com>
2024-10-16 02:36:16 +0300

    add

--- src/components/mainComponents/web3.js
+++ src/components/mainComponents/web3.js
@@ -3,22 +3,28 @@ import Web3 from 'web3';
 let web3;
 
 const connectWallet = async () => {
-    window.location.href = "https://metamask.app.link/dapp/https://front-hack-vtb.vercel.app/";
-    if (window.ethereum) {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  
+    if (isMobile) {
+      const mobileLink = "https://metamask.app.link/dapp/https://front-hack-vtb.vercel.app/";
+      window.location.href = mobileLink;
+    } else {
+      if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
         try {
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            console.log("Подключение успешно!");
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
+          console.log("Подключение успешно!");
         } catch (error) {
-            console.error("Пользователь отклонил доступ к кошельку.");
+          console.error("Пользователь отклонил доступ к кошельку.");
         }
-    } else if (window.web3) {
+      } else if (window.web3) {
         window.web3 = new Web3(web3.currentProvider);
-    } else {
+      } else {
         console.error("MetaMask не установлен.");
+      }
     }
-    return web3
-}
-
+  
+    return web3;
+  }
 
 export default connectWallet;
